9446f286c0b728a462401b94c97a743620ff1a5e
Olukayode <olukayode.adebiyi@hotmail.co.uk>
2021-05-29 21:47:35 -0400

    View Advert Page

---
+++ src/app/adverts/view-advert/view-advert.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ViewAdvertComponent } from './view-advert.component';
+
+describe('ViewAdvertComponent', () => {
+  let component: ViewAdvertComponent;
+  let fixture: ComponentFixture<ViewAdvertComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ViewAdvertComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ViewAdvertComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/adverts/view-advert/view-advert.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-advert',
+  templateUrl: './view-advert.component.html',
+  styleUrls: ['./view-advert.component.css']
+})
+export class ViewAdvertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
