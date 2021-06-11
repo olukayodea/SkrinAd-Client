718202711416b17c75316ac3271c693b626ee071
Olukayode <olukayode.adebiyi@hotmail.co.uk>
2021-06-11 01:16:26 -0400

    Login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Validators, FormBuilder } from '@angular/forms';
+import { Title, Meta } from '@angular/platform-browser';
 import { Params, Router, ActivatedRoute } from '@angular/router';
 import { environment } from 'src/environments/environment';
 import { ErrorCodes } from '../_models/data';
@@ -48,6 +49,8 @@ export class LoginComponent implements OnInit {
   get confirmPassword() { return this.passwordForm.get('confirmPassword'); }
 
   constructor(
+    private titleService: Title,
+    private meta: Meta,
     private router: Router,
     private fb: FormBuilder,
     private activatedRoute: ActivatedRoute,
@@ -55,6 +58,11 @@ export class LoginComponent implements OnInit {
     private apiService: ApiService,
     private EncrDecr: EncrDecrService
   ) {
+    this.titleService.setTitle("Login");
+    this.meta.addTags([
+      { name: 'deacription', content: "Login to your SkrinAd clients account" },
+      { name: 'Keywords', content: "SkrinAd, CLients, Login,  non-intrusive, lifestyle, multimedia contents, get paid, advertistment, surveys, money, smart business decisions, Advertise, Advertise with us" },
+    ]);
     this.getRouteParams();
     this.errorCodes = new ErrorCodes();
   }
@@ -162,34 +170,34 @@ export class LoginComponent implements OnInit {
     this.apiService.setPassword(password, this.tempToken).subscribe(
       user => {
         let randKey = this.checkService.getRandomString(32);
-        // if (user.success == true) {
+        if (user.success == true) {
 
-        //   // Save user to Local storage
+          // Save user to Local storage
 
-        //   var currentTime = Math.floor((Date.now() / 1000) + (60 * 10));
+          var currentTime = Math.floor((Date.now() / 1000) + (60 * 10));
           
-        //   var keydata = {
-        //     key: randKey,
-        //     expire: currentTime.toString()
-        //   }
+          var keydata = {
+            key: randKey,
+            expire: currentTime.toString()
+          }
 
-        //   localStorage.setItem('key', btoa( JSON.stringify(keydata)) );
-        //   localStorage.setItem('userData', this.EncrDecr.set(environment.localKey+randKey, JSON.stringify(user.data)));
+          localStorage.setItem('key', btoa( JSON.stringify(keydata)) );
+          localStorage.setItem('userData', this.EncrDecr.set(environment.localKey+randKey, JSON.stringify(user.data)));
 
-        //   this.redirect = localStorage.getItem('route');
+          this.redirect = localStorage.getItem('route');
 
-        //   // if (this.redirect !== undefined) {     
-        //   //   this.redirect = user.data.mainPage
-        //   // }
+          // if (this.redirect !== undefined) {     
+          //   this.redirect = user.data.mainPage
+          // }
           
-        //   window.location.href = decodeURIComponent(this.redirect);
-        //   // this.router.navigate(['/'+decodeURIComponent(this.redirect)]);
-        // } else {
-        //   this.errorMsg = true;
-        //   this.error = user.error.message;
-        //   this.passwordButtonText = "Change";
-        //   this.processing = false;
-        // }
+          window.location.href = decodeURIComponent(this.redirect);
+          // this.router.navigate(['/'+decodeURIComponent(this.redirect)]);
+        } else {
+          this.errorMsg = true;
+          this.error = user.error.message;
+          this.passwordButtonText = "Change";
+          this.processing = false;
+        }
       }
     );
   }
